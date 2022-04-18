8aba71c991298489152950f762ad8cb45a97ad7b
Dasterio <103887185+Dasterio@users.noreply.github.com>
2022-04-19 02:56:47 +0800

    Create 3.1.cpp

---
+++ 3.1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, d;
+    ofstream r2d2("c3po");
+    cout<<"Введите A и B";
+    cin>>a>>b;
+    if (a>b){
+        c=a;
+        d=b;
+        } else if (b>a){
+            c=b;
+            d=a;
+            } else {
+                cout<<"Число А равен В. Введите число заного";
+            }
+    for(d;d<=c;d++){
+        if (c==d){
+        r2d2<<(d*3);
+        } else {
+            r2d2<<(d*3)<<"\n";;
+        }
+    }
+    r2d2.close();    
+   return 0;
+}
