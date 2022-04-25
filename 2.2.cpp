0bf84a58fc978e8ff4f6c588006a4c7f61c4ff77
Dasterio <103887185+Dasterio@users.noreply.github.com>
2022-04-26 01:47:38 +0800

    Update 2.2.cpp

--- 2.2.cpp
+++ 2.2.cpp
@@ -4,16 +4,18 @@ using namespace std;
 
 int main()
 {
-    int a,b;
-    for (a=1; a<1000; a=a+4){
-        if (a % 7) {
-            continue;
-        } else {
-            b=b+a;
-        }
-        
+    int i = 1;
+    int s = 0;
+    while (i < 1001)
+    {
+           
+        if (i % 7 == 0) { 
+            cout << i << '\n';
+            s=i+s;
+       }  
+
+       i = i + 4;
     }
-    cout<<"Сумма чисел равна ";
-    cout<<b;
+    cout << "Сумма  равна "<< s << '\n';
     return 0;
 }
