c21721766625bcbab868ad3156ca7cc50937ac83
Dasterio <103887185+Dasterio@users.noreply.github.com>
2022-04-26 00:57:55 +0800

    Update 2.1.cpp

--- 2.1.cpp
+++ 2.1.cpp
@@ -4,14 +4,12 @@ using namespace std;
 
 int main()
 {
-    int a;
-    while (a<100){
-        if (a % 3 == 0) {
-            cout<< "Число делиться на 3: "<< a << "\n";
+    for(int i = 0 ; i <= 100 ; i++)
+    {
+        if (i % 3 == 0)
+        {
+        cout<<i<<endl;
         }
-        a++;
     }
-
-
     return 0;
 }
