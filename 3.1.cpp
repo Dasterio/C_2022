4e50d33022183f9fbafc4e17f1e218f485a266bc
Dasterio <103887185+Dasterio@users.noreply.github.com>
2022-04-26 02:24:54 +0800

    Update 3.1.cpp

--- 3.1.cpp
+++ 3.1.cpp
@@ -5,26 +5,13 @@ using namespace std;
 
 int main()
 {
-    int a, b, c, d;
-    ofstream r2d2("c3po");
-    cout<<"Введите A и B";
-    cin>>a>>b;
-    if (a>b){
-        c=a;
-        d=b;
-        } else if (b>a){
-            c=b;
-            d=a;
-            } else {
-                cout<<"Число А равен В. Введите число заного";
-            }
-    for(d;d<=c;d++){
-        if (c==d){
-        r2d2<<(d*3);
-        } else {
-            r2d2<<(d*3)<<"\n";;
-        }
+    int a, b;
+    
+    ofstream GG("Dasti");
+    cin >> a >> b;
+    for (int i = a; i <= b ; i++)
+    {
+        GG << i * 3 << "\n";
     }
-    r2d2.close();    
-   return 0;
+    GG.close();
 }
